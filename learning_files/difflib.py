426c4a0467ef4159dd372fac7c430c5639bae12b
inyong <scodein00@gmail.com>
2024-11-20 00:29:32 +0900

    practice python difflib module

---
+++ learning_files/difflib.py
@@ -0,0 +1,9 @@
+s = SequenceMatcher(
+    lambda x: x == " ",
+    "private Thread currentThread;",
+    "private volatile Thread currentThread;",
+)
+print(round(s.ratio(), 3))
+
+for block in s.get_matching_blocks():
+    print("a[%d] and b[%d] match for %d elements" % block)
